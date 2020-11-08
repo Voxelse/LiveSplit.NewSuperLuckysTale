016b6b44f75d34fbc3e01921c747b7c37994d2d2
Voxelse <voxelse@gmail.com>
2020-11-08 02:37:01 +0100

    Add Jinx split + game pass version

--- NewSuperLuckysTaleMemory.cs
+++ NewSuperLuckysTaleMemory.cs
@@ -6,17 +6,19 @@ using System.Collections.Generic;
 namespace LiveSplit.NewSuperLuckysTale {
     public class NewSuperLuckysTaleMemory : Memory {
 
-        Pointer<IntPtr> loading;
-        StringPointer level;
+        private Pointer<IntPtr> loading;
+        private StringPointer level;
+        private Pointer<bool> cutscene;
 
         private bool isReady = true;
+        private int cutsceneCount = 0;
 
         private readonly RemainingDictionary remainingSplits;
 
         private readonly MonoHelper mono;
-        
+
         public NewSuperLuckysTaleMemory(LiveSplitState state, Logger logger) : base(state, logger) {
-            processName = "nslt";
+            SetProcessNames("nslt", "New Super Lucky's Tale");
             remainingSplits = new RemainingDictionary(logger);
             mono = new MonoHelper(this);
         }
@@ -31,6 +33,8 @@ namespace LiveSplit.NewSuperLuckysTale {
 
                 level = ptrFactory.MakeString("GameScenesInterface", "_persistentLevelName", 0x14);
                 level.StringType = EStringType.UTF16Sized;
+
+                cutscene = ptrFactory.Make<bool>("CutsceneSequence", "<HasActiveCutscene>k__BackingField");
 
                 Logger.Log(ptrFactory.ToString());
             });
@@ -50,15 +54,31 @@ namespace LiveSplit.NewSuperLuckysTale {
 
         public override void OnStart(HashSet<string> splits) {
             remainingSplits.Setup(splits);
+            cutsceneCount = 0;
         }
 
         public override bool Split() {
-            return remainingSplits.Count != 0 && (SplitLevels());
+            return remainingSplits.Count != 0 && (SplitLevels() || SplitCutscenes());
 
             bool SplitLevels() {
                 return remainingSplits.ContainsKey("Level") && level.Changed
                     && (level.New.StartsWith("Chapter") || String.IsNullOrEmpty(level.New))
                     && remainingSplits.Split("Level", level.Old);
+            }
+
+            bool SplitCutscenes() {
+                if(!remainingSplits.ContainsKey("Cutscene")) { return false; }
+
+                // Remove if more cutscenes are added
+                if(!level.New.Equals("SP_Boss_01", StringComparison.Ordinal)) { return false; }
+
+                if(level.Changed) {
+                    cutsceneCount = 0;
+                }
+                if(!cutscene.Old && cutscene.New) {
+                    return remainingSplits.Split("Cutscene", level.New + "_" + (++cutsceneCount));
+                }
+                return false;
             }
         }
 
