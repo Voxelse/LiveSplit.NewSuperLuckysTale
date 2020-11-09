e102f132bf60083175ac6d900bcc0f15b90858c2
Voxelse <voxelse@gmail.com>
2020-11-09 06:55:24 +0100

    Add Foxington splits

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6BB2E506-5F97-4650-9B75-9643713E594C")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
